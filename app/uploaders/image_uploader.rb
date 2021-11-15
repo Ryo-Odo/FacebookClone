d73727651b2e541202bebcc26db6d4289e784c5e
Ryo_Odo <marukae@gmail.com>
2021-11-15 04:34:42 +0000

    エラーページ編集

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
+  include CarrierWave::MiniMagick
   storage :file # 保存形式の設定。他に使うものとしてはfog形式などがある。
-  #process :resize_to_limit => [50, 50] # 画像サイズの調整
+  process :resize_to_limit => [50, 50] # 画像サイズの調整
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
